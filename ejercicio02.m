5df5ccaad203262eaac2d4e970d82a09b538463a
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2018-01-18 22:00:55 +0100

    Ejercicio2 empezado

--- ejercicio02.m
+++ ejercicio02.m
@@ -1,12 +1,33 @@
-f = @(x) x.^7 - x - cos(x) - 1
+clear all
 
-df = @(x) 7 * x.^6 -1 + sen(x)
+clf
+
+clc
+
+f = @(x) x.^7 - x - cos(x) + 1
+
+df = @(x) 7 * x.^6 -1 + sin(x)
+
+d2f = @(x) 42 * x.^5 + cos(x)
 
 ejex = @(x) 0 * x;
 
 tolerancia = 10 ^ -5
 
-I = [0:0.05:1];
+I = [-1.5:0.01:1.5];
 
+%subplot(3, 1, 1)
+figure
 plot(I, f(I), I, ejex(I))
-axis equal
+
+%subplot(3, 1, 2)
+figure
+plot(I, df(I), I, ejex(I))
+
+%subplot(3, 1, 3)
+figure
+plot(I, d2f(I), I, ejex(I))
+
+
+%x01 = 1;
+%[x, niter] = puntofijo2(df, x01, tolerancia, maxIter)
