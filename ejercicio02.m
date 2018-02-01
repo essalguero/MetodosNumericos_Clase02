a233c60e268eb2b2594fb2633f84b5fe2c233c2f
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2018-02-01 01:58:31 +0100

    no message

--- ejercicio02.m
+++ ejercicio02.m
@@ -14,6 +14,8 @@ ejex = @(x) 0 * x;
 
 tolerancia = 10 ^ -5
 
+nmax = 2000
+
 I = [-1.5:0.01:1.5];
 
 %subplot(3, 1, 1)
@@ -31,3 +33,11 @@ plot(I, d2f(I), I, ejex(I))
 
 %x01 = 1;
 %[x, niter] = puntofijo2(df, x01, tolerancia, maxIter)
+
+x0_1 = -0.5;
+
+x0_2 = 0.5
+
+[x, niter] = puntofijo2(df, x0_1, tolerancia, nmax)
+
+[x, niter] = puntofijo2(df, x0_2, tolerancia, nmax)
